eeecab5bab550be59b0eeaf2ace2b00390d7e89e
Hamza <hamzaahmad6292@gmail.com>
2024-01-26 18:41:12 +0500

    3

---
+++ Models/videoModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  thumbnailUrl: {
+    type: String,
+    required: true,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  uploader: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model
+    required: true,
+  },
+});
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = Video;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongodb = require("./DB.js")
+mongodb()
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin',"http://localhost:3000" ),
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+})
+app.get("/", (req, res) => {
+    res.send("Hamza The Great")
+})
+app.use(express.json())
+app.use('/api', require("./Routes/Create_user"))
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+
+})
