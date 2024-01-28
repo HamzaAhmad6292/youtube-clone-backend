7595f9973be1b0ea7a06f6ec721a9bdb9171931d
Hamza <hamzaahmad6292@gmail.com>
2024-01-28 19:24:08 +0500

    hmm

---
+++ Routes/upload_Video.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+import Video from "../Models/videoModel";
+
+
+
+router.post("")
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const mongodb = require("./DB.js")
 mongodb()
 
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin',"http://localhost:3000" ),
+    res.header('Access-Control-Allow-Origin',"http://localhost:3001" ),
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
     res.send("Hamza The Great")
 })
 app.use(express.json())
-app.use('/api', require("./Routes/Create_user"))
+app.use('/', require("./Routes/Create_user"))
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 
